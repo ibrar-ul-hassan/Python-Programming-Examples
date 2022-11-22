7061ef0205c3e4febdc181f464c42fa30e4cae31
ibrar-ul-hassan <ibrarulhassan967@gmail.com>
2022-11-22 22:18:31 +0500

    Basic Programs

---
+++ Basic Programs/Python program to add two numbers.py
@@ -0,0 +1,14 @@
+# Given two numbers num1 and num2. The task is to write a Python program to find the addition of these two numbers.
+
+# Input: num1 = 5, num2 = 3
+# Output: 8
+
+# Input: num1 = 13, num2 = 6
+# Output: 19
+
+first_number = 8
+second_number = 99
+
+sum = first_number + second_number
+
+print(sum)
