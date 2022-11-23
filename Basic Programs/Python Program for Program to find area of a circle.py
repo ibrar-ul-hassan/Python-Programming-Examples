ab39229b6215666aaa43ecf8507a098c69902fb4
ibrar-ul-hassan <ibrarulhassan967@gmail.com>
2022-11-23 20:51:02 +0500

    how to calcaulate the area of circle

---
+++ Basic Programs/Python Program for Program to find area of a circle.py
@@ -0,0 +1,11 @@
+# The area of a circle can simply be evaluated using the following formula.
+
+# Area = pi * r2
+# where r is radius of circle 
+
+def area_of_cirle(r):
+    pi = 3.14
+    return pi * (r**2)
+
+num = 4
+print(area_of_cirle(4))
